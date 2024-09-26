9dd90bac559ea10165bdd76476fcd0143a994d0c
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-09-26 16:13:23 +0200

    basic card layout it is not finished only html

--- src/lib/Member.svelte
+++ src/lib/Member.svelte
@@ -2,8 +2,33 @@
     export let data;
 </script>
 
-{#each data.members as member}
-    <ul>
-        <li>{member.title}</li>
-    </ul>
-{/each}
+<section>
+
+    {#each data.members as member}
+        <ul>
+            <li>
+                service design
+            </li>
+            <li>{member.title}</li>
+            <li>amsterdam</li>
+            <li>25 tot 49 werkemers</li>
+        </ul>
+    {/each}
+</section>
+
+<style>
+    section{
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+        gap: 2em;
+    }
+    ul{
+        list-style: none;
+        padding: 0;
+        margin: 0;
+        width: 100%;
+        overflow: hidden;
+        border: var(--grey) 1px solid;
+    }
+
+</style>
--- src/routes/members/+page.svelte
+++ src/routes/members/+page.svelte
@@ -10,13 +10,6 @@
 
     <div  class="header">
     <img  src="{myImage}" class="logo" alt="Dutch Digital Agencies logo link naar home" loading="lazy">
-        <svg id="hamburger" class="Header__toggle-svg" viewbox="0 0 60 40">
-            <g stroke="#fff" stroke-width="4" stroke-linecap="round" stroke-linejoin="round">
-                <path id="top-line" d="M10,10 L50,10 Z"></path>
-                <path id="middle-line" d="M10,20 L50,20 Z"></path>
-                <path id="bottom-line" d="M10,30 L50,30 Z"></path>
-            </g>
-        </svg>
     </div>
     <a href="/">Home</a>
 
@@ -27,9 +20,10 @@
         Ook DDA lid worden? Ga dan naar deze pagina voor meer informatie.
     </p>
 
-    <form>
+    <form class="searchform">
         <label for="site-search">zoek een bureau:</label>
-        <input type="search" name="search" id="site-search"/>
+        <input type="search" name="search" id="site-search" placeholder="zoek een bureau..."/>
+        <hr>
         <button>zoek een bureach</button>
     </form>
 
@@ -44,87 +38,20 @@
         padding: 2em;
     }
     .header{
-        /*background-color: var(--grey);*/
+        background-color: var(--grey);
+    }
+    #site-search{
+        border: none;
+        /*border-bottom: 2px solid var(--grey);*/
+    }
+    .searchform{
+        border-bottom: var(--grey);
+
     }
 
     .logo{
         width: 12em;
     }
 
-/*    overgenomen van https://codepen.io/cassie-codes/pen/abmvqKO werkt alleen niet*/
-    #hamburger{
-        background-color: var(--black);
-    }
-
-    #top-line,
-    #bottom-line,
-    #middle-line {
-        transform-box: fill-box;
-        transform-origin: center;
-    }
-
-    /*svg:hover {*/
-
-        #top-line svg:hover {
-            animation: down-rotate 0.6s ease-out both;
-        }
-
-
-        #bottom-line svg:hover {
-            animation: up-rotate 0.6s ease-out both;
-        }
-        #middle-line svg:hover {
-            animation: hide 0.6s ease-out forwards;
-        }
-
-    @keyframes up-rotate {
-        0% {
-            animation-timing-function: cubic-bezier(0.16, -0.88, 0.97, 0.53);
-            transform: translateY(0px);
-        }
-        30% {
-            transform-origin: center;
-            animation-timing-function: cubic-bezier(0.34, 1.56, 0.64, 1);
-            transform: translateY(-10px);
-        }
-        100% {
-            transform-origin: center;
-            transform: translateY(-10px) rotate(45deg) scale(0.9);
-        }
-    }
-
-    @keyframes down-rotate {
-        0% {
-            animation-timing-function: cubic-bezier(0.16, -0.88, 0.97, 0.53);
-            transform: translateY(0px);
-        }
-        30% {
-            transform-origin: center;
-            animation-timing-function: cubic-bezier(0.34, 1.56, 0.64, 1);
-            transform: translateY(10px);
-        }
-        100% {
-            transform-origin: center;
-            transform: translateY(10px) rotate(-45deg) scale(0.9);
-        }
-    }
-
-    @keyframes hide {
-        29% {
-            opacity: 1;
-        }
-        30% {
-            opacity: 0;
-        }
-        100% {
-            opacity: 0;
-        }
-    }
-
-    @media (min-width: 30em)  {
-       #hamburger{
-           display: none;
-       }
-    }
 
 </style>

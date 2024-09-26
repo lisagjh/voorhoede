8b505292f56a9932f8256300eb8ac02cddb7b080
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-09-26 16:52:37 +0200

    build: Member cards there is a layout for mobile tablet and desktop the styling must be more exact with the design

--- src/lib/Member.svelte
+++ src/lib/Member.svelte
@@ -2,10 +2,27 @@
     export let data;
 </script>
 
-<section>
-
+<div>
     {#each data.members as member}
         <ul>
+
+            <li>
+                <!--{#if member.photo && member.photo.id }-->
+<!--                    <picture>-->
+
+<!--                        <img src="https://fdnd-agency.directus.app/assets/{member.photo.id}" alt="{member.title}"-->
+<!--                             width="{member.photo.width}" height="{member.photo.height}">-->
+<!--                    </picture>-->
+
+                <picture>
+                        <img src="https://fdnd-agency.directus.app/assets/687ea821-1626-4d43-bd4a-16d7a7bb1f34?format=avif" alt="{member.title}"
+                             width="{member.photo.width}" height="{member.photo.height}">
+                    </picture>
+
+
+
+                <!--{/if}-->
+               </li>
             <li>
                 service design
             </li>
@@ -15,21 +32,28 @@
             <li>details</li>
         </ul>
     {/each}
-</section>
+</div>
 
 <style>
-    section{
+    div{
         display: grid;
         grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-        gap: 2em;
+        /*gap: 2em;*/
+        border-top: 1px solid var(--grey);
+        border-bottom: 1px solid var(--grey);
+        /*padding: 2em;*/
     }
     ul{
         list-style: none;
-        padding: 0;
         margin: 0;
         width: 100%;
         overflow: hidden;
         border: var(--grey) 1px solid;
+        padding: 2em;
+    }
+    img{
+        object-fit: cover;
+        width: 100%;
     }
 
 </style>

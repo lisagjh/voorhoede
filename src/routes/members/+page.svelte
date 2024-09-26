797720bd78316118eebc4b706ffb4f5c31791061
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-09-26 11:24:13 +0200

    feature: i have setup a connection to the database and added data from the table dda agencies vacancies

--- src/routes/members/+page.svelte
+++ src/routes/members/+page.svelte
@@ -19,4 +19,27 @@
 
     {/each}
 
+
+    <h2>vacatuer data</h2>
+    {#each data.vacature as member}
+
+        <ul>
+
+
+            <li>{member.Title}  title</li>
+            <li>{member.Hours}  uren</li>
+            <li>{member.description} omschrijiving</li>
+            <li>{member.Data_Posted } datum gepost</li>
+        </ul>
+        <hr>
+
+    {/each}
+
 </main>
+
+<style>
+    main{
+        line-height: 1.5;
+        padding: 2em;
+    }
+</style>

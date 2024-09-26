f2383fe9738264ef917601cba87d431c86f7c661
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-09-26 15:21:52 +0200

    refactor code for readability

--- src/lib/Vacancies.svelte
+++ src/lib/Vacancies.svelte
@@ -2,10 +2,7 @@
     export let data;
 </script>
 {#each data.vacature as member}
-
     <ul>
-
-
         <li>{member.Title}  title</li>
         <li>{member.Hours}  uren</li>
         <li>{member.description} omschrijiving</li>

61b66f1d85b2498006b0eed4a6762c20cce14e4a
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-09-27 14:31:49 +0200

    added styling cards to the member page for mobile tablet desktop

--- src/lib/Member.svelte
+++ src/lib/Member.svelte
@@ -1,9 +1,95 @@
 <script>
     export let data;
 </script>
+<div class="gridcontainer">
+    {#each data.members as member}
+        <div class="card">
+                <picture>
+                    <img src="https://fdnd-agency.directus.app/assets/687ea821-1626-4d43-bd4a-16d7a7bb1f34?format=avif"
+                         alt="{member.title}"
+                         width="{member.photo.width}" height="{member.photo.height}">
+                </picture>
+        <ul class="cardlabel">
+            <li class="labelfilters">
+                service design
+            </li>
+            <li class="labelfilters">label</li>
+        </ul>
+        <ul>
+            <li class="Companyname">{member.title}</li>
+            <li>amsterdam</li>
+        </ul>
+        <ul class="cardlabel">
+            <li>25 tot 49 werkemers</li>
+            <li class="card__li__hiring">hiring</li>
+        </ul>
+        <ul>
+            <li class="linkdetails">details</li>
+        </ul>
+        </div>
+    {/each}
 
-{#each data.members as member}
-    <ul>
-        <li>{member.title}</li>
-    </ul>
-{/each}
+</div>
+
+<style>
+    .gridcontainer {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+        /*gap: 2em;*/
+        border-top: 1px solid var(--grey);
+        border-bottom: 1px solid var(--grey);
+        text-transform: uppercase;
+    }
+
+    ul {
+        list-style: none;
+        margin: 0;
+    }
+
+    .card {
+
+        width: 100%;
+        overflow: hidden;
+        border: var(--grey) 1px solid;
+        padding: 2rem;
+        display: grid;
+        grid-template-rows: auto auto auto auto auto;
+        gap: 16px;
+    }
+    .card__li__hiring{
+        background-color: var(--blue);
+        color: var(--white);
+        padding: 2px;
+        height: 1.6rem;
+    }
+
+    img {
+        object-fit: cover;
+        width: 100%;
+    }
+
+    .cardlabel {
+        display: flex;
+        flex-direction: row;
+        text-overflow: ellipsis;
+        gap: 1rem;
+        height: 2rem;
+    }
+
+    .labelfilters {
+        border: 2px solid var(--black);
+        width: max-content;
+        padding: 3px;
+    }
+
+    .Companyname {
+        font-size: 48px;
+    }
+
+    .linkdetails {
+        color: var(--blue);
+        font-weight: bold;
+
+    }
+
+</style>

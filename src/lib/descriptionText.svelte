f869acb87c8ef5151c04ead6189cb2bcdf35f603
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-09-30 18:24:21 +0200

    feature: added backhome Description components to the member page

---
+++ src/lib/descriptionText.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let label = '';
+</script>
+
+<p>
+    {label}
+</p>
+
+<style>
+    p {
+        text-align: center;
+    }
+</style>
--- src/routes/members/+page.svelte
+++ src/routes/members/+page.svelte
@@ -2,21 +2,16 @@
     export let data;
 
     import Member from "$lib/Member.svelte";
-    import Vacancies from "$lib/Vacancies.svelte";
-    import myImage from '../../lib/images/logo.png';
     import Searchform from "$lib/Searchform.svelte";
+    import Intro from "$lib/descriptionText.svelte";
+    import Backhome from "$lib/BackHomeBtn.svelte";
 </script>
 
 <main>
 
-    <a href="/">Home</a>
 
-    <h2>onze leden</h2>
-    <p>
-        De [nummer] beste digitale bureaus van Nederland zijn verenigd in Dutch Digital Agencies. Zet de juiste filters
-        aan en zoek contact met het bureau dat het beste bij past bij jouw vraagstuk en uitdaging. 
-        Ook DDA lid worden? Ga dan naar deze pagina voor meer informatie.
-    </p>
+    <Backhome/>
+    <Intro/>
     <Searchform/>
 
     <Member data={data}/>

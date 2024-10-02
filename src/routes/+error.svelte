9228da2850fa7804dd714e2c1675b091693349a1
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-10-02 15:01:28 +0200

    built: i added text what the user must do when there is a error

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,8 +2,15 @@
     import { page } from '$app/stores'
     console.log($page.status)
 </script>
+<section>
     <h1>Er gaat iets mis zie onderstaande error?</h1>
     <p>{$page.status} - {$page.error.message}</p>
+
+    <p><a href="/">
+        ga terug naar home
+    </a>  </p>
+</section>
+
 
 <style>
     section{

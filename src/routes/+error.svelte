3916c592973472cd21d454071f7ab6f25bb093da
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-10-03 12:35:41 +0200

    i added a background color on the text ga terug naar home making the texxt more of a button

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,17 +4,30 @@
 </script>
 <section>
     <h1>Er gaat iets mis zie onderstaande error?</h1>
-    <p>{$page.status} - {$page.error.message}</p>
-
-    <p><a href="/">
+    <p>{$page.status} - {$page.error.message}
+    </p>
+    <a href="/">
         Ga terug naar home
-    </a>  </p>
+    </a>
+
+
 </section>
 
 
 <style>
+
+
     section{
         padding: 2rem;
 
+    }
+    p{
+        margin-bottom: 2rem;
+    }
+    a{
+        background-color: var(--blue);
+        text-decoration: none;
+        padding: 1rem;
+        color: var(--white);
     }
 </style>

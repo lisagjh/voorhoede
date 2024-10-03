5596b09a92672d5ddb4e84cb8ca430b0fd7ce7cb
Jason <jay.jpy.wong@gmail.com>
2024-10-03 13:15:20 +0200

    Feature : base of component implemented

---
+++ src/lib/Link.svelte
@@ -0,0 +1,35 @@
+<script>
+    //In +pave.svelte kun je de link dus aanpassen dit is een soort property
+    export let href="";
+    export let clazz="";
+</script>
+
+
+<a href={href} class={clazz}>
+
+    <slot name="svg-icon-left"/>
+
+    <slot name="link-text">
+        <!--Fallback-->
+        <span>Link</span>
+    </slot>
+
+    <slot name="svg-icon-right"/>
+
+</a>
+
+<style>
+
+    a {
+        display: flex;
+        align-items: center;
+        color: inherit;
+        text-decoration: none;
+        font-size: .75em;
+        text-transform: uppercase;
+        gap: 5px; /* incase someone forgets to type in uppercase */
+    }
+
+    /* Different link styles for each page ? */ 
+
+</style>
--- src/routes/vacatures/+page.svelte
+++ src/routes/vacatures/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import Text from "$lib/Text.svelte";
     import BackToBtn from "$lib/BackToBtn.svelte";
+    import Link from "$lib/Link.svelte";
 </script>
 
 <main>
@@ -12,9 +13,18 @@
 
     <section class="intro-section">
         
-        <BackToBtn href="/">
+        <Link href="/" clazz="back-to-home">
+
+            <svg fill="#000000" slot="svg-icon-left"
+            width="14px" height="14px" viewBox="0 0 52 52" enable-background="new 0 0 52 52" xml:space="preserve">
+            <path d="M48.6,23H15.4c-0.9,0-1.3-1.1-0.7-1.7l9.6-9.6c0.6-0.6,0.6-1.5,0-2.1l-2.2-2.2c-0.6-0.6-1.5-0.6-2.1,0
+            L2.5,25c-0.6,0.6-0.6,1.5,0,2.1L20,44.6c0.6,0.6,1.5,0.6,2.1,0l2.1-2.1c0.6-0.6,0.6-1.5,0-2.1l-9.6-9.6C14,30.1,14.4,29,15.3,29
+            h33.2c0.8,0,1.5-0.6,1.5-1.4v-3C50,23.8,49.4,23,48.6,23z"/>
+            </svg>
+
             <span slot="link-text">TERUG NAAR HOME</span>
-        </BackToBtn>
+
+        </Link>
 
         <Text>
             <h1 slot="page-title">Vacatures</h1>

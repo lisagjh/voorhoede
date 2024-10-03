d2284b011259ce22dcee6b492399028251dce522
Jason <jay.jpy.wong@gmail.com>
2024-10-03 22:20:43 +0200

    Feature : Component created and HTML written.

---
+++ src/lib/Vacancy.svelte
@@ -0,0 +1,23 @@
+<script>
+    
+    import Link from "$lib/Link.svelte";
+
+</script>
+
+<article>
+    <slot name="job-title" class=""/>
+    <slot name="language" class=""/>
+    <slot name="company" class=""/>
+    <slot name="city" class=""/>
+    <slot name="link" class=""/>
+</article>
+
+<style>
+
+article {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 1em;
+}
+
+</style>

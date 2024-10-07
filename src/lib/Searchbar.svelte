bc34ad53dc0ccfa85c76f3078e6e3f8eb1924c6e
Jason <jay.jpy.wong@gmail.com>
2024-10-07 16:08:39 +0200

    Feature
    Searchbar v1 functionality

---
+++ src/lib/Searchbar.svelte
@@ -0,0 +1,43 @@
+<script>
+    export let placeholderText = "";
+    export let searchTerm="";
+ 
+</script>
+ 
+<!--preventDefault zorgt ervoor dat het form niet verstuurd wordt-->
+<form action="#" method="GET" on:submit|preventDefault>
+<input type="search" name="" 
+    class="search-bar" placeholder={placeholderText}
+    aria-label="Search" bind:value={searchTerm}
+    />
+</form>
+ 
+ 
+<style>
+ 
+.search-bar {
+  min-width: 304px;
+  padding: 1em 2px;
+  margin: 1em;
+  border: none;
+  border-bottom: 1.35px solid var(--black);
+  font-size: 1em;
+  outline: var(--grey);
+}
+ 
+ul {
+  margin: .5em 0em 0em 2em;
+}
+ 
+.search-bar::placeholder {
+  color: var(--grey)
+}
+ 
+input[type="search"]::-webkit-search-cancel-button {
+  -webkit-appearance: none;
+  height: .5em;
+  width: .5em;
+  background: rgb(238, 92, 7);
+}
+ 
+</style>
--- src/routes/members/+page.svelte
+++ src/routes/members/+page.svelte
@@ -2,10 +2,20 @@
     export let data;
 
     import Member from "$lib/Member.svelte";
-    import Searchform from "$lib/Searchform.svelte";
     import Intro from "$lib/descriptionText.svelte";
     import Backhome from "$lib/BackHomeBtn.svelte";
     import Header from "$lib/Header.svelte";
+    import Searchbar from "$lib/Searchbar.svelte";
+
+    // Input van de gebruiker 
+    let searchTerm = "";
+
+  // $: Is een dynamisch element wat zich instant aanpast op de data die erin zit. 
+  // .filter is een built in JS functie die een nieuwe arraay aanmaakt met de juiste items op basis van de requirements die er
+  // na volgen onder andere .ToLowerCase en .Includes met de member array waar op dit moment alle agencies in staan.
+  $: filteredMembers = data.members.filter(member =>
+    member.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 </script>
 
 <Header/>
@@ -16,12 +26,17 @@
         label=" De [nummer] beste digitale bureaus van Nederland zijn verenigd in Dutch Digital Agencies. Zet de juiste filters aan en zoek contact met het bureau dat het beste bij past bij jouw vraagstuk en uitdaging.
         'Ook DDA lid worden? Ga dan naar deze pagina voor meer informatie."
     />
-    <Searchform/>
+    
+    <!--To build : een delay functie, instant feedback is misschien niet user friendlyg-->
+    <Searchbar
+        placeholderText="Zoek een bureau"
+        bind:searchTerm={searchTerm}
+    />
 
-    <Member data={data}/>
-
-</main>
-
+    <!--Het door pasen van de filteredMembers array naar members zodat die alleen de gefilterde items laat zien-->
+    <Member data={{ members: filteredMembers }}/>
+      
+    </main>
 <style>
     main {
         line-height: 1.5;
@@ -36,6 +51,5 @@
     .logo {
         width: 12em;
     }
-
 
 </style>

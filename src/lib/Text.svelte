35804c6ce86cfaa3358ba95656c2dd021df8d3d1
Jason <jay.jpy.wong@gmail.com>
2024-10-09 16:33:29 +0200

    Feature : unused script and style tag removed

--- src/lib/Text.svelte
+++ src/lib/Text.svelte
@@ -1,14 +1,4 @@
-<script>
-    // add class props here if needed so we can style each text element differently
-    
-</script>
 
     <slot name="page-title"/>
 
     <slot name="page-description"/> 
-
-<!--More text elements if needed-->
-
-<style>
-
-</style>

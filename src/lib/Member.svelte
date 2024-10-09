5ece0a3ed91fd467d0512cc3a4e728f84b0d015c
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-10-09 10:20:55 +0200

    refactor:change code and remove unnacessery code

--- src/lib/Member.svelte
+++ src/lib/Member.svelte
@@ -1,42 +1,46 @@
 <script>
     import Link from "$lib/Link.svelte";
+
     export let data;
 
 </script>
 
 
 <article class="grid-container">
-
-
     {#each data.members as member}
+
         <article class="card">
             <picture>
                 <source srcset="https://fdnd-agency.directus.app/assets/{member.photo}?format=avif" type="image/avif">
                 <source srcset="https://fdnd-agency.directus.app/assets/{member.photo}?format=webp" type="image/webp">
                 <img src="https://fdnd-agency.directus.app/assets/{member.photo}" loading="lazy" alt="">
             </picture>
+
             <ul class="card-label-filters">
                 <li class="label-filters">
                     service design
                 </li>
                 <li class="label-filters">label</li>
             </ul>
-                <h2 class="company-name">{member.title}</h2>
-                <p>{member.address}</p>
+
+            <h2>{member.title}</h2>
+            <p>{member.address}</p>
 
             <ul class="card-label">
                 <li>{parseInt(member.colleagues)}  werknemers</li>
                 <li class="card-hiring">hiring</li>
 
-
             </ul>
 
-            <Link href="/" clazz="detail-link"><span slot="link-text">Details</span><svg width="16" height="16" slot="svg-icon-right"viewBox="0 0 24 24" fill="none"><path d="M6 12H18M18 12L13 7M18 12L13 17" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></Link>
+            <Link href="/" clazz="detail-link"><span slot="link-text">Details</span>
+                <svg width="16" height="16" slot="svg-icon-right" viewBox="0 0 24 24" fill="none">
+                    <path d="M6 12H18M18 12L13 7M18 12L13 17" stroke-width="2" stroke-linecap="round"
+                          stroke-linejoin="round"/>
+                </svg>
+            </Link>
 
         </article>
     {/each}
-
-
 
 
 </article>
@@ -47,19 +51,8 @@
         grid-template-columns: repeat(auto-fill, minmax(263px, 1fr));
         text-transform: uppercase;
     }
-    h2{
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        overflow: hidden;
-    }
-
-    ul {
-        list-style: none;
-        margin: 0;
-    }
 
     .card {
-
         width: 100%;
         overflow: hidden;
         border: var(--grey) 1px solid;
@@ -69,19 +62,7 @@
         gap: 16px;
     }
 
-    .card-hiring {
-        background-color: var(--blue);
-        color: var(--white);
-        padding: 2px;
-        height: 1.6rem;
-    }
-
-    img {
-        object-fit: cover;
-        width: 100%;
-        height: 100%;
-    }
-    .card-label-filters{
+    .card-label-filters {
         display: flex;
         flex-direction: row;
         text-overflow: ellipsis;
@@ -103,9 +84,32 @@
         padding: 3px;
     }
 
-    .company-name {
+    h2 {
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        overflow: hidden;
         font-size: 38px;
     }
+
+    ul {
+        list-style: none;
+        margin: 0;
+    }
+
+
+    .card-hiring {
+        background-color: var(--blue);
+        color: var(--white);
+        padding: 2px;
+        height: 1.6rem;
+    }
+
+    img {
+        object-fit: cover;
+        width: 100%;
+        height: 100%;
+    }
+
 
     @media (max-width: 350px) {
 
@@ -114,13 +118,16 @@
             grid-template-columns: repeat(auto-fill, minmax(263px, 1fr));
             text-transform: uppercase;
         }
-       img{
-           width: 92%;
-       }
-        .card{
+
+        img {
+            width: 92%;
+        }
+
+        .card {
             padding: 1rem;
         }
-        .card{
+
+        .card {
             grid-template-rows: 16rem auto auto auto auto;
             grid-template-columns: 14em;
             row-gap: 2em;
@@ -138,7 +145,7 @@
 
 
     @media (min-width: 825px) {
-        .card{
+        .card {
             grid-template-rows: 16rem auto auto 4rem auto;
         }
 

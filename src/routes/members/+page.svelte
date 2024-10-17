9320a7cd4b08d23a05e9c0c72943f5cdf800e7ec
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-10-17 11:20:15 +0200

    i added aria label for accesibility

--- src/routes/members/+page.svelte
+++ src/routes/members/+page.svelte
@@ -30,7 +30,7 @@
     </Link>
 
     <Text>
-        <h1 slot="page-title">Leden</h1>
+        <h1 aria-label="Leden" slot="page-title">Leden</h1>
         <p slot="page-description">
             De [nummer] beste digitale bureaus van Nederland zijn verenigd in Dutch Digital Agencies. Zet de juiste
             filters aan en zoek contact met het bureau dat het beste bij past bij jouw vraagstuk en uitdaging.

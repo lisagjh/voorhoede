d60f7ca83c8562dad4d453890a76f799ec18ea1a
lisagjh <lisagjhoorn@outlook.com>
2024-10-23 11:44:07 +0200

    update intro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,8 @@
   import IntroSection from "./../lib/IntroSection.svelte";
 </script>
 
-<IntroSection title="Home" displayHomeLink=false>
+<IntroSection title="De beste
+digitale bureaus van nederland" displayHomeLink=false>
   <!-- svelte:fragment ipv p -->
-  <svelte:fragment slot="subtitle">subtitle</svelte:fragment>
+  <svelte:fragment slot="subtitle">Dutch Digital Agencies (DDA) verenigt en verbindt toonaangevende digitale bureaus. Samen maken we de wereld elke dag een beetje slimmer.</svelte:fragment>
 </IntroSection>

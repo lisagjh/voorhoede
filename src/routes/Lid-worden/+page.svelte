941677890f692cd89409431fd4de740b2df893d4
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-11-27 11:29:59 +0100

    i added form and subrisce button

---
+++ src/routes/Lid-worden/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+
+
+  import Link from "$lib/Link.svelte";
+  import IntroSection from "$lib/IntroSection.svelte";
+
+  import Form from "$lib/Form.svelte";
+
+
+</script>
+
+<!--http://localhost:5173/Lid-worden-->
+<IntroSection
+        title="Inschrijven"
+        subtitle=""
+/>
+
+<section>
+  <Form/>
+</section>
+
+<style>
+
+</style>

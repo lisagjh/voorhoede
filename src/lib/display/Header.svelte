e776cea7041feb6e1e91fd217401876810025e60
lisagjh <lisagjhoorn@outlook.com>
2024-12-07 20:00:21 +0100

    FIX #118 respsonsive nav

--- src/lib/display/Header.svelte
+++ src/lib/display/Header.svelte
@@ -1,23 +1,38 @@
 <script>
-  import Logo from "$lib/display/Logo.svelte"
-  import Nav from "$lib/navigation/Nav.svelte";
-</script>
-
-<header>
+    import Logo from "$lib/display/Logo.svelte";
+    import Nav from "$lib/navigation/Nav.svelte";
+  </script>
+  
+  <header>
     <Logo />
-    <Nav/>
-</header>
-
-<style>
+    <Nav />
+  </header>
+  
+  <style>
     header {
-        font-family: var(--martian-mono);
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: space-between;
-        overflow-x: hidden;
-        overflow: hidden;
-        padding: 0.5rem 1rem;
-        width: 100%;
+      font-family: var(--martian-mono);
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      overflow-x: hidden;
+      overflow: hidden;
+      padding: 0.5rem 1rem;
+      width: 100%;
+      height: 100%;
     }
-</style>
+  
+    @media (width > 50rem) {
+      header {
+        display: flex;
+        flex-direction: column;
+        position: fixed;
+        left: 0;
+        top: 0;
+        width: 16%;
+        height: 100%;
+        max-height: 100vh;
+        padding: 1rem;
+        justify-content: flex-start;
+      }
+    }
+  </style>

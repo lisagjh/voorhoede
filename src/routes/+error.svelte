36bd743359698663584383f14de3587328e05e47
Jason <jay.jpy.wong@gmail.com>
2025-01-10 13:49:20 +0100

    Comments converted to english or removed

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -31,7 +31,7 @@
   </a>
   <h1>Er gaat iets mis, zie onderstaande error.</h1>
   <p>{$page.status} - {$page.error.message}
-      <!--    page status is de statuscode -->
+      <!--Pagestatus is the statuscode-->
   </p>
 
 </section>
@@ -49,18 +49,18 @@
 
   @media (max-width: 35em) {
       h1 {
-          font-size: 1.5rem; /*letters worden anders te groot*/
+          font-size: 1.5rem; 
       }
   }
 
   @media (min-width: 35em) {
       h1 {
-          font-size: 2rem; /*letters worden anders te groot*/
+          font-size: 2rem; 
           line-height: 1.5;
       }
   }
   h1{
-      letter-spacing: 2px; /* de tekst staat te dicht tegen elkaar aan*/
+      letter-spacing: 2px; 
   }
 
   a {
@@ -80,14 +80,13 @@
   }
 
   p {
-      /*margin zodat de 404 error niet te dicht tegen de terug naar home tekst staat*/
       margin-bottom: 2rem;
   }
 
   svg {
-      vertical-align: middle; /*dit zorgt ervoor dat de pijl en de tekst op dezelfde hoogte staan */
+      vertical-align: middle; 
       margin-bottom: 0.2rem;
       color: var(--black);
-      transform: scaleX(-1); /*met deze code gaat het pijltje naar links     */
+      transform: scaleX(-1); 
   }
 </style>

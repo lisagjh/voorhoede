3f1cb5651bed283a67ff0359f2b4aa052479e29c
yujing-student <100352887+yujing-student@users.noreply.github.com>
2025-01-13 14:58:28 +0100

    remove unncassery whitespace

--- src/routes/members/+page.server.js
+++ src/routes/members/+page.server.js
@@ -1,10 +1,8 @@
 import fetchJson from "$lib/fetch-json.js";
 
-
 export async function load() {
     const ddaAgencies = 'https://fdnd-agency.directus.app/items/dda_agencies/'
     const members = await fetchJson(ddaAgencies)
-
 
     return {
         members: members.data,

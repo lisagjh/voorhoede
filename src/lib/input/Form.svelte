a0e26bb3003c4d2bb0fd828005f36820a7a30023
yujing-student <100352887+yujing-student@users.noreply.github.com>
2025-01-21 10:25:56 +0100

    Build: I added the css on the form element of the button subsribe

--- src/lib/input/Form.svelte
+++ src/lib/input/Form.svelte
@@ -20,12 +20,40 @@
         <Select/>
         <Textarea/>
 <!--        todo classname add for button of the styling-->
-        <Button
+        <div class="button-styling-border">
 
+
+        <Button
+                className="button-test"
                 tag="button"
-                label="Inschrijven"
+                label="SCHRIJF MIJ IN"
                 href="/bedankt"
+
         />
+
+        <svg aria-label="pijltje ga naar schrijf me in"
+             width="16"
+             height="16"
+             viewBox="0 0 16 16"
+             fill="none"
+             xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                    d="M8 3L13.0001 8.0001L8 13.0002"
+                    stroke="#222226"
+                    stroke-width="1.5"
+                    stroke-miterlimit="4.62023"
+            />
+            <path
+                    d="M3 8L13 8"
+                    stroke="#222226"
+                    stroke-width="1.5"
+                    stroke-miterlimit="4.62023"
+                    stroke-linejoin="round"
+            />
+        </svg>
+        </div>
+
     </fieldset>
 </form>
 
@@ -63,6 +91,56 @@
             gap: 1rem;
 
         }
+    }
+
+
+    /*button component inschrijven*/
+
+    /*.button-styling-border{*/
+    /*    border-left: 1px solid var(--grey);*/
+    /*    border-right: 1px solid var(--grey);*/
+    /*    border-top: 1px solid var(--grey);*/
+    /*    border-bottom: 1px solid var(--grey);*/
+    /*    margin-right: -0.3rem;*/
+    /*    color: var(--black);*/
+    /*    text-decoration: none;*/
+    /*    padding: 0px 0.2rem 0.4rem 0.4rem;*/
+    /*    background-color: transparent;*/
+    /*    margin-top: 2rem;*/
+    /*    grid-row: 8;*/
+    /*}*/
+
+
+
+    .button-test{
+        border-left: 1px solid var(--grey);
+        border-right: 1px solid var(--grey);
+        border-top: 1px solid var(--grey);
+        border-bottom: 1px solid var(--grey);
+        margin-right: -0.3rem;
+        color: var(--black);
+        text-decoration: none;
+        padding: 0px 0.2rem 0.4rem 0.4rem;
+        background-color: transparent;
+        margin-top: 2rem;
+        display: grid;
+        grid-row: 8;
+    }
+    .button-test *{
+        box-sizing: content-box;
+    }
+    .button-test:hover{
+        color: var(--blue);
+    }
+
+    button-test:focus {
+        outline: 2px solid var(--blue);
+    }
+
+    svg{
+        /*margin-bottom: -0.6rem;*/
+        border-left: 1px solid var(--grey);
+        /*padding: 0.2rem 0.2rem 0.4rem 0.4em;*/
     }
 
 </style>

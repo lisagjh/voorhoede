3700368ab9a5229be66f60387e91247b6de7e1ea
yujing-student <100352887+yujing-student@users.noreply.github.com>
2025-01-21 15:50:17 +0100

    change let to const on page.server.js

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,31 +1,31 @@
 import fetchJson from "$lib/fetch-json.js";
 
 export async function load() {
-  let ddaVacancies =
+  const ddaVacancies =
     "https://fdnd-agency.directus.app/items/dda_agencies_vacancies";
-  let ddaAgencies = "https://fdnd-agency.directus.app/items/dda_agencies/";
+  const ddaAgencies = "https://fdnd-agency.directus.app/items/dda_agencies/";
 
-  let vacatures = await fetchJson(ddaVacancies);
-  let agencies = await fetchJson(ddaAgencies);
+  const vacatures = await fetchJson(ddaVacancies);
+  const agencies = await fetchJson(ddaAgencies);
 
   // Access the `data` property from both API responses
-  let allVacatures = vacatures.data;
+  const allVacatures = vacatures.data;
 
-  let agencyData = agencies.data;
+  const agencyData = agencies.data;
 
   // Create a Map for efficient agency lookups
-  let agencyMap = new Map(
+  const agencyMap = new Map(
     agencies.data.map((agency) => [agency.id, agency.title])
   );
 
   // Enrich vacancies with agency names
-  let vacancyWithAgencyData = allVacatures.map((vacancy) => ({
+  const vacancyWithAgencyData = allVacatures.map((vacancy) => ({
     ...vacancy,
     agencyName: agencyMap.get(vacancy.agency_id) || "Unknown Agency",
   }));
 
   // Take the first 6 vacancies for latest items
-  let lastFiveItems = vacancyWithAgencyData.slice(-6).reverse();
+  const lastFiveItems = vacancyWithAgencyData.slice(-6).reverse();
 
   console.log("Server received data:", allVacatures.length);
 

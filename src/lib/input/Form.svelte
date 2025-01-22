967a5fccee574c09b5104b8d1e351d96e59b5a33
yujing-student <100352887+yujing-student@users.noreply.github.com>
2025-01-22 10:37:38 +0100

    tring to use snippet

--- src/lib/input/Form.svelte
+++ src/lib/input/Form.svelte
@@ -34,6 +34,55 @@
                 tag="button"
                 type="submit"
         />
+
+        <!--{#snippet svg(svgIcon)}-->
+        <!--    <svg aria-label="pijltje ga naar schrijf me in"-->
+        <!--         width="16"-->
+        <!--         height="16"-->
+        <!--         viewBox="0 0 16 16"-->
+        <!--         fill="none"-->
+        <!--         xmlns="http://www.w3.org/2000/svg"-->
+        <!--    >-->
+        <!--        <path-->
+        <!--                d="M8 3L13.0001 8.0001L8 13.0002"-->
+        <!--                stroke="#222226"-->
+        <!--                stroke-width="1.5"-->
+        <!--                stroke-miterlimit="4.62023"-->
+        <!--        />-->
+        <!--        <path-->
+        <!--                d="M3 8L13 8"-->
+        <!--                stroke="#222226"-->
+        <!--                stroke-width="1.5"-->
+        <!--                stroke-miterlimit="4.62023"-->
+        <!--                stroke-linejoin="round"-->
+        <!--        />-->
+        <!--    </svg>-->
+        <!--{/snippet}-->
+        <!--{#if svgIcon}-->
+        <!--    <svg-->
+        <!--            width="16"-->
+        <!--            height="16"-->
+        <!--            viewBox="0 0 16 16"-->
+        <!--            fill="none"-->
+        <!--            xmlns="http://www.w3.org/2000/svg"-->
+        <!--    >-->
+        <!--        <path-->
+        <!--                d="M8 3L13.0001 8.0001L8 13.0002"-->
+        <!--                stroke="#222226"-->
+        <!--                stroke-width="1.5"-->
+        <!--                stroke-miterlimit="4.62023"-->
+        <!--        />-->
+        <!--        <path-->
+        <!--                d="M3 8L13 8"-->
+        <!--                stroke="#222226"-->
+        <!--                stroke-width="1.5"-->
+        <!--                stroke-miterlimit="4.62023"-->
+        <!--                stroke-linejoin="round"-->
+        <!--        />-->
+        <!--    </svg>-->
+        <!--{/if}-->
+
+
     </fieldset>
 </form>
 
